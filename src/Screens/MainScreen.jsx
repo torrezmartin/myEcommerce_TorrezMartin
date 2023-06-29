daed200765936f0207c2c6ec860369dafb202fa4
tyn1994 <torrezmartin.developer@gmail.com>
2023-06-29 16:39:56 -0300

    Clase 4

--- src/Screens/MainScreen.jsx
+++ src/Screens/MainScreen.jsx
@@ -1,23 +1,74 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
-import { StatusBar } from 'expo-status-bar'
+import { Button, StyleSheet, Text, TextInput, View, FlatList, Modal, Pressable } from 'react-native'
+import React, { useState } from 'react'
+
+const renderItemTask = ({item}) => {
+    return <View style={styles.task} key={item.id}>
+            <Text>{item.task}</Text>
+        </View>
+}
 
 const MainScreen = ({ taskList }) => {
+    const [list, setList] = useState(taskList);
+    const [input, setInput] = useState("");
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const onAddTask = () => {
+        setList([
+            ...list, 
+            {
+                id: list.length + 1,
+                task: input,
+                completed: false
+            }
+        ]);
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.view1}>
-                <TextInput style={styles.inputStyle} />
+                <TextInput
+                    style={styles.inputStyle}
+                    placeholder='Ingrese un ítem'
+                    value={input}
+                    onChangeText={setInput}
+                    />
                 <Button
-                    title='Agregar tarea'
+                    title='Agregar'
+                    onPress={onAddTask}
                 />
             </View>
             <View style={styles.view2}>
-                {taskList.map(item => {
-                    return <View style={styles.task} key={item.id}>
-                        <Text>{item.task}</Text>
-                    </View>
-                })}
+                <FlatList
+                style={styles.flatList}
+                data={list}
+                keyExtractor={item => item.id}
+                renderItem={renderItemTask}
+                />
+                <Pressable
+                    style={[styles.button, styles.buttonOpen]}
+                    onPress={() => setModalVisible(true)}>
+                    <Text style={styles.textStyle}>Show Modal</Text>
+                </Pressable>
             </View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                Alert.alert('Modal has been closed.');
+                setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                    <Text style={styles.modalText}>Hello World!</Text>
+                    <Pressable
+                    style={[styles.button, styles.buttonClose]}
+                    onPress={() => setModalVisible(!modalVisible)}>
+                    <Text style={styles.textStyle}>Hide Modal</Text>
+                    </Pressable>
+                </View>
+                </View>
+            </Modal>
         </View>
     )
 }
@@ -57,8 +108,54 @@ const styles = StyleSheet.create({
         marginRight: 10
     },
     task: {
-        width: '80%',
+        width: '100%',
         backgroundColor: 'azure',
         padding: 10,
-    }
+        margin: 10
+    },
+    flatList: {
+        width: '90%',
+    },
+
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+      },
+      modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      },
+      button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+      },
+      buttonOpen: {
+        backgroundColor: '#F194FF',
+      },
+      buttonClose: {
+        backgroundColor: '#2196F3',
+      },
+      textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+      modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+      },
 })
